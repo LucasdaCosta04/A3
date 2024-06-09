630ad617a68960b4da4c30f28d9595c0366423ab
LucasdaCosta04 <164957384+LucasdaCosta04@users.noreply.github.com>
2024-06-09 11:36:18 -0300

    feat: Adiciona classe Principal
    Esta alteração adiciona a classe Principal ao projeto. A classe Principal servirá como a classe principal do programa, a partir da qual a execução do aplicativo será iniciada. A classe Principal pode conter métodos de inicialização, configuração e outras operações necessárias para o funcionamento do programa.

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,13 @@
+
+package principal;
+    
+import visao.frmMenuPrincipal;
+    
+    public class Principal {
+        public static void main(String[] args) {
+    // Instancia a interface gráfica
+         frmMenuPrincipal objetotela = new frmMenuPrincipal();
+    // Torna a janela visível
+          objetotela.setVisible(true);
+        }
+    }
