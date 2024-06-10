c3e124c1fca4b706a3c30cfd4088bcafc924004e
matheus <seifertvictor71@gmail.com>
2024-06-10 01:02:25 -0300

    Update e fix: Atualizaçao da interface grafica de cadastro amigo e correçao de bug
    - Interface mais estilizada 
    - Correçao do bug onde nao abria a tela por conta das imagens

--- src/main/java/visao/frmCadastroAmigo.java
+++ src/main/java/visao/frmCadastroAmigo.java
@@ -4,17 +4,27 @@
  */
 package visao;
 
+import javax.swing.JOptionPane;
+import modelo.Amigo;
+import dao.AmigoDAO;
+
 /**
  *
  * @author User
  */
 public class frmCadastroAmigo extends javax.swing.JFrame {
 
+    private Amigo objetoAmigo;
+    private AmigoDAO amigoDAO;
+
     /**
      * Creates new form frmGerenciaAmigos
      */
     public frmCadastroAmigo() {
         initComponents();
+        this.objetoAmigo = new Amigo();
+        this.amigoDAO = new AmigoDAO();
+
     }
 
     /**
@@ -26,96 +36,147 @@ public class frmCadastroAmigo extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jTituloCadastroAmigo = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        JTFNome = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        JTFNumero = new javax.swing.JTextField();
-        JBCancelar = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
         JBCadastrar = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        JBCancelar = new javax.swing.JButton();
+        JTFNome = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        JTFTelefone = new javax.swing.JTextField();
+        jTituloCadastroAmigo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Cadastrar Amigo");
-        setMinimumSize(new java.awt.Dimension(590, 365));
+        setMinimumSize(new java.awt.Dimension(595, 365));
         getContentPane().setLayout(null);
 
-        jTituloCadastroAmigo.setFont(new java.awt.Font("Trebuchet MS", 1, 25)); // NOI18N
-        jTituloCadastroAmigo.setText("Cadastrar Amigo");
-        getContentPane().add(jTituloCadastroAmigo);
-        jTituloCadastroAmigo.setBounds(182, 31, 190, 30);
+        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
+        jPanel1.setMinimumSize(new java.awt.Dimension(583, 330));
+        jPanel1.setLayout(null);
 
-        jLabel2.setFont(new java.awt.Font("Trebuchet MS", 1, 14)); // NOI18N
-        jLabel2.setText("Nome:");
-        getContentPane().add(jLabel2);
-        jLabel2.setBounds(6, 106, 50, 18);
+        jPanel2.setBackground(new java.awt.Color(0, 102, 102));
+        jPanel2.setLayout(null);
 
-        JTFNome.setForeground(new java.awt.Color(204, 204, 204));
-        JTFNome.setText("Insira o nome :");
-        JTFNome.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                JTFNomeActionPerformed(evt);
-            }
-        });
-        getContentPane().add(JTFNome);
-        JTFNome.setBounds(6, 130, 450, 30);
-
-        jLabel3.setFont(new java.awt.Font("Trebuchet MS", 1, 14)); // NOI18N
-        jLabel3.setText("Numero :");
-        getContentPane().add(jLabel3);
-        jLabel3.setBounds(6, 173, 70, 18);
-
-        JTFNumero.setForeground(new java.awt.Color(204, 204, 204));
-        JTFNumero.setText("Insira o numero de telefone:");
-        getContentPane().add(JTFNumero);
-        JTFNumero.setBounds(6, 197, 430, 30);
-
-        JBCancelar.setFont(new java.awt.Font("Trebuchet MS", 1, 12)); // NOI18N
-        JBCancelar.setForeground(new java.awt.Color(255, 0, 0));
-        JBCancelar.setText("Cancelar");
-        JBCancelar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                JBCancelarActionPerformed(evt);
-            }
-        });
-        getContentPane().add(JBCancelar);
-        JBCancelar.setBounds(365, 280, 110, 40);
-
+        JBCadastrar.setBackground(new java.awt.Color(51, 255, 0));
         JBCadastrar.setFont(new java.awt.Font("Trebuchet MS", 1, 12)); // NOI18N
-        JBCadastrar.setForeground(new java.awt.Color(0, 204, 0));
+        JBCadastrar.setForeground(new java.awt.Color(255, 255, 255));
         JBCadastrar.setText("Cadastrar");
         JBCadastrar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 JBCadastrarActionPerformed(evt);
             }
         });
-        getContentPane().add(JBCadastrar);
-        JBCadastrar.setBounds(89, 280, 110, 40);
+        jPanel2.add(JBCadastrar);
+        JBCadastrar.setBounds(430, 260, 110, 40);
 
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/imagem amigo.png"))); // NOI18N
-        getContentPane().add(jLabel4);
-        jLabel4.setBounds(390, 120, 200, 220);
+        JBCancelar.setBackground(new java.awt.Color(255, 153, 0));
+        JBCancelar.setFont(new java.awt.Font("Trebuchet MS", 1, 12)); // NOI18N
+        JBCancelar.setForeground(new java.awt.Color(255, 255, 255));
+        JBCancelar.setText("Voltar");
+        JBCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBCancelarActionPerformed(evt);
+            }
+        });
+        jPanel2.add(JBCancelar);
+        JBCancelar.setBounds(30, 260, 110, 40);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/Imagem Engrenagem.png"))); // NOI18N
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(-40, -20, 300, 240);
+        JTFNome.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JTFNomeActionPerformed(evt);
+            }
+        });
+        jPanel2.add(JTFNome);
+        JTFNome.setBounds(10, 130, 450, 30);
+
+        jLabel2.setFont(new java.awt.Font("Trebuchet MS", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Nome:");
+        jPanel2.add(jLabel2);
+        jLabel2.setBounds(10, 100, 60, 21);
+
+        jLabel3.setFont(new java.awt.Font("Trebuchet MS", 1, 18)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Telefone:");
+        jPanel2.add(jLabel3);
+        jLabel3.setBounds(10, 170, 90, 21);
+
+        JTFTelefone.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JTFTelefoneActionPerformed(evt);
+            }
+        });
+        jPanel2.add(JTFTelefone);
+        JTFTelefone.setBounds(10, 200, 450, 30);
+
+        jTituloCadastroAmigo.setFont(new java.awt.Font("Trebuchet MS", 1, 25)); // NOI18N
+        jTituloCadastroAmigo.setForeground(new java.awt.Color(255, 204, 0));
+        jTituloCadastroAmigo.setText("Cadastrar Amigo");
+        jPanel2.add(jTituloCadastroAmigo);
+        jTituloCadastroAmigo.setBounds(160, 30, 200, 30);
+
+        jPanel1.add(jPanel2);
+        jPanel2.setBounds(10, 10, 560, 310);
+
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(0, 0, 580, 330);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void JTFNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFNomeActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_JTFNomeActionPerformed
-
     private void JBCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBCancelarActionPerformed
-     this.dispose();
+        this.dispose();
         // TODO add your handling code here:
     }//GEN-LAST:event_JBCancelarActionPerformed
 
     private void JBCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBCadastrarActionPerformed
-        // TODO add your handling code here:
+
+            try {
+            String nome = "";
+            String fone = "";
+            if (this.JTFNome.getText().length() < 2) {
+                throw new Mensagens("Nome deve conter ao menos 2 caracteres.");
+            } else {
+                nome = this.JTFNome.getText();
+            }
+            if (this.JTFTelefone.getText().length() < 2) {
+                throw new Mensagens("Telefone deve conter ao menos 2 caracteres.");
+            } else {
+                fone = this.JTFTelefone.getText();
+            }
+            // envia os dados para o Controlador cadastrar
+            Amigo novoAmigo = new Amigo();
+            novoAmigo.setNome(nome);
+            novoAmigo.setTelefone(fone);
+
+            AmigoDAO amigoDAO = new AmigoDAO();
+            boolean inserido = amigoDAO.inserirAmigoBD(novoAmigo);
+
+            if (inserido) {
+                JOptionPane.showMessageDialog(null, "Amigo Cadastrado com Sucesso!");
+                // limpa campos da interface
+                this.JTFNome.setText("");
+                this.JTFTelefone.setText("");
+            } else {
+                JOptionPane.showMessageDialog(null, "Erro ao cadastrar amigo.");
+            }
+            // Exibindo no console o amigo cadastrado
+            System.out.println(this.objetoAmigo.getMinhaLista().toString());
+        } catch (Mensagens erro) {
+            JOptionPane.showMessageDialog(null, erro.getMessage());
+        } catch (NumberFormatException erro2) {
+            JOptionPane.showMessageDialog(null, "Informe um número válido.");
+        }
     }//GEN-LAST:event_JBCadastrarActionPerformed
+
+    private void JTFTelefoneActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFTelefoneActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JTFTelefoneActionPerformed
+
+    private void JTFNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFNomeActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JTFNomeActionPerformed
 
     /**
      * @param args the command line arguments
@@ -157,11 +218,11 @@ public class frmCadastroAmigo extends javax.swing.JFrame {
     private javax.swing.JButton JBCadastrar;
     private javax.swing.JButton JBCancelar;
     private javax.swing.JTextField JTFNome;
-    private javax.swing.JTextField JTFNumero;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JTextField JTFTelefone;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JLabel jTituloCadastroAmigo;
     // End of variables declaration//GEN-END:variables
 }
